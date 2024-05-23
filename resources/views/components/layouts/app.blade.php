c3b7a735e4b514af9e6175c8433b9000fe45609f
Crashneitor14 <pizzagames14@gmail.com>
2024-05-22 20:48:50 -0400

    mensaje publicacion

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,8 +13,11 @@
 <body class="antialiased bg-slate-350">
 
     <x-layouts.nav />
+
     @if(session('status'))
-    <div> {{session('status')}} </div>
+    <div class="max-w-screen-xl px-3 py-2 mx-auto font-bold text-white sm:px-6 lg:px-8 bg-emerald-500 dark:bg-emerald-700">
+        {{session('status')}}
+    </div>
     @endif
 
 {{ $slot }}
