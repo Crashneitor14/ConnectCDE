e9ffc31f1ecaa77eb50cb355814b1d68b11dceff
Crashneitor14 <pizzagames14@gmail.com>
2024-07-08 17:41:27 -0400

    avance login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Faker\Guesser\Name;
 use Illuminate\Http\Request;
 
 class RegisteredUserController extends Controller
@@ -10,8 +11,13 @@ class RegisteredUserController extends Controller
     public function store(Request $request){
 
 
+        //ver validaciones
+        $request->validate([
+            'name' =>['required','string','max:255'],
+            'email' =>['required','string','email','max:255','unique:users'],
+            'password'=>['required','confirmed',]
 
-        return $request;
+        ]);
 
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,74 @@
 
 <form class="max-w-xl px-8 py-4 mx-auto bg-slate-300 rounded shadow"action="{{route('register')}}" method="POST">
     @csrf
-    {{--@include('posts.form')--}}
+
+    <div class="space-y-4">
+        <label class="flex flex-col">
+            <span class="flex justify-center font-serif text-slate-600">
+                Nombre Estudiante
+            </span>
+            {{--Nombre--}}
+            <input class="rounded-md shadow-sm border-slate-250 focus:ring-slate-300 focus:ring-opacity-50 focus:border-slate-300"
+                name="name"
+                type="text"
+                value="{{ old('name')}}"
+            >
+
+            @error('name')
+                <small class="font-bold text-red-500/80">{{$message}}</small>
+            @enderror
+
+        </label>
+        {{--Correo--}}
+        <label class="flex flex-col">
+            <span class="flex justify-center font-serif text-slate-600">
+                Correo Institucional
+            </span>
+            <input class="rounded-md shadow-sm border-slate-250 focus:ring-slate-300 focus:ring-opacity-50 focus:border-slate-300"
+                name="email"
+                type="email"
+                value="{{ old('email')}}"
+            >
+
+            @error('email')
+                <small class="font-bold text-red-500/80">{{$message}}</small>
+            @enderror
+
+        </label>
+        {{--Contraseña--}}
+        <label class="flex flex-col">
+            <span class="flex justify-center font-serif text-slate-600">
+                Contraseña
+            </span>
+            <input class="rounded-md shadow-sm border-slate-250 focus:ring-slate-300 focus:ring-opacity-50 focus:border-slate-300"
+                name="password"
+                type="password"
+                value="{{ old('password')}}"
+            >
+
+            @error('password')
+                <small class="font-bold text-red-500/80">{{$message}}</small>
+            @enderror
+
+        </label>
+        {{--Contraseña confirmar--}}
+        <label class="flex flex-col">
+            <span class="flex justify-center font-serif text-slate-600">
+                Confirmar Contraseña
+            </span>
+            <input class="rounded-md shadow-sm border-slate-250 focus:ring-slate-300 focus:ring-opacity-50 focus:border-slate-300"
+                name="password_confirmation"
+                type="password"
+                value="{{ old('password_confirmation')}}"
+            >
+
+            @error('password_confirmation')
+                <small class="font-bold text-red-500/80">{{$message}}</small>
+            @enderror
+
+        </label>
+    </div>
+
 
     <div class="flex items-center justify-between mt-4">
     <button class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase transition duration-150 ease-in-out border-2 border-transparent rounded-md bg-sky-800 hover:bg-sky-700 active:bg-sky-700 focus:outline-none focus:border-sky-500" type="submit">Registrar</button>
