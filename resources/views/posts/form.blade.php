a8bf21d3b87093f85776687d4d0ecb3777b7b115
Crashneitor14 <pizzagames14@gmail.com>
2024-07-10 18:30:21 -0400

    standby auth

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -28,5 +28,15 @@
     @enderror
 
 </label>
+<label class="flex flex-col">
+    <span class="flex justify-center font-serif text-slate-600">Detalles del cuerpo</span>
+    <textarea class="rounded-md shadow-sm border-slate-250 focus:ring-slate-300 focus:ring-opacity-50 focus:border-slate-300" name="details" rows="3">{{old('details',$post->details)}}</textarea>
+
+    @error('details') {{--detector de errores--}}
+    <small class="font-bold text-red-500/80">{{$message}}</small>
+    @enderror
+
+</label>
+
 </div>
 
