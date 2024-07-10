64a0300662d3cb0abc24f5de1a050ab1255e753f
Crashneitor14 <pizzagames14@gmail.com>
2024-07-10 15:58:15 -0400

    revisando publicaciones

--- resources/views/components/layouts/nav.blade.php
+++ resources/views/components/layouts/nav.blade.php
@@ -6,7 +6,11 @@
     <li><a href="{{ route('contact')}}">contacto</a></li>
 </ul>
 </nav>
+
+NAVEGADOR
+
 --}}
+
 
 <nav
     class="w-screen bg-white border-b white:bg-slate-900 border-slate-900/10 lg:px-8 white:border-slate-300/10 lg:mx-0">
@@ -16,23 +20,10 @@
                 <div class="flex items-center flex-shrink-0">
                     <a href="{{ route('menu')}}">
                         <img class="flex items-center justify-center" src="{{ asset('images/ubb.png') }}" alt="Mi Imagen" width="67">
-                        {{--
-                        <svg class="w-8 h-8 text-sky-500" fill="none" width="0" stroke="currentColor"
-                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                            <path
-                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z">
-                            </path>
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222">
-                            </path>
-                        </svg>
-                    --}}
                     </a>
                 </div>
                 <div class="mx-auto">
                     <div class="flex space-x-4">
-                        <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
                         <a href="{{ route('menu')}}"
                             class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-dark {{ request()->routeIs('menu') ? 'text-sky-600 ' : 'text-slate-250'}}">
                             Home
@@ -41,14 +32,10 @@
                         class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-dark {{ request()->routeIs('posts.*') ? 'text-sky-600 ' : 'text-slate-250'}}">
                         Publicaciones
                     </a>
-                        <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
                         <a href="{{ route('plata')}}"
                             class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-dark {{ request()->routeIs('plata') ? 'text-sky-600 ' : 'text-slate-250'}}">
                             Finanzas
                         </a>
-                        <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
-
-                        <!-- Active: 'text-sky-600 dark:text-white', Inactive 'text-slate-400' -->
                         <a href="{{ route('contact')}}"
                             class="px-3 py-2 text-sm font-medium rounded-md hover:text-sky-600 dark:hover:text-dark {{ request()->routeIs('contact') ? 'text-sky-600 ' : 'text-slate-250'}}">
                             Contacto
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,12 +7,12 @@
     <a class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase ease-in-out border border-transparent rounded-md dark:text-sky-200 bg-sky-800"
         href="{{route('posts.create')}}">Crear nueva publicacion</a>
 </header>
-    <main class="grid w-full gap-8 px-96 max-w-7xl sm:grid-cols-1 md:grid-cols-1">
+<main class="m-auto grid w-full gap-8 px-96 max-w-7xl sm:grid-cols-1 md:grid-cols-1">
    @foreach ($posts as $post)
 
    <div class=" bg-white max-w-3xl px-4 py-2 space-y-4 bg-grey rounded shadow bg-slate-280">
        <h2 class="text-xl text-slate-600 dark:text-slate-600 hover:underline">
-            <a href="{{route('posts.show' , $post)}}">
+            <a href="{{route('posts.show' , $post)}}" class="flex justify-center">
                 {{ $post->title}}
 
 
@@ -22,8 +22,9 @@
             {{$post->body}}
         </div>
         <div class="flex justify-between ">
-            <a class="inline-flex items-center text-xs font-semibold tracking-widest text-center uppercase transition duration-150 ease-in-out dark:text-slate-600 text-slate-500 hover:text-slate-600 dark:hover:text-slate-600 focus:outline-none focus:border-slate-200"
-                href="{{route('posts.edit', $post)}}">Editar</a>
+            <a class="inline-flex items-center font-semibold tracking-widest text-center uppercase transition duration-150 ease-in-out  text-slate-600 hover:text-slate-600 dark:hover:text-slate-600 focus:outline-none focus:border-slate-200"
+                href="{{route('posts.edit', $post)}}">Editar
+            </a>
             <form action="{{ route('posts.destroy', $post) }}" method="POST">
                 @csrf
                 @method('DELETE')
