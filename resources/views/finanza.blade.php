928be9c03119708c1b4a684e364ba8b53eeecb09
Crashneitor14 <pizzagames14@gmail.com>
2024-10-20 22:14:35 -0300

    avance finanzas act DB

--- resources/views/finanza.blade.php
+++ resources/views/finanza.blade.php
@@ -10,12 +10,6 @@
     @endauth
 </header>
 
-
-
-
-
-
-
 <div class="flex justify-center mt-8">
     <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
         <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
@@ -26,7 +20,9 @@
                         <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Detalles</th>
                         <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Estado</th>
                         <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Monto</th>
-                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Fecha Inicio</th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Fecha Fin</th>
+                        <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"></th>
                     </tr>
                 </thead>
 
@@ -41,17 +37,25 @@
                         </td>
 
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <a href="">Detalles de actividad gastos</a>
+                            <a>Detalles de actividad gastos</a>
                         </td>
 
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <a href="">Poner estado actividad</a>
+                            <a>Poner estado actividad</a>
+                        </td>
+
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <a>Fecha inicio</a>
+                        </td>
+
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <a>Fecha fin</a>
                         </td>
 
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Monto asignado o gastado</td>
 
                         <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Editar</a>
+                            <a class="text-indigo-600 hover:text-indigo-900">Ver Detalles</a>
                         </td>
                     </tr>
                 </tbody>
@@ -59,6 +63,9 @@
         </div>
     </div>
 </div>
+
+
+
 </x-layouts.app>
 
 
