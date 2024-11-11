d10d4005e66cd70ecf68716a5addf931885c4a2a
Crashneitor14 <pizzagames14@gmail.com>
2024-11-11 18:28:21 -0300

    actividades nombre

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -3,7 +3,7 @@
     meta-description="Finanza meta description"
 >
 <header class="px-6 py-4 space-y-2 text-center">
-    <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500"> Boletas Cobranza</h1>
+    <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500"> Actividades - Cobranza</h1>
     @auth
     <a class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase ease-in-out border border-transparent rounded-md dark:text-sky-200 bg-sky-800"
         href="{{route('act.create')}}">Iniciar nueva actividad</a>
