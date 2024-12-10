685a446764c4f2faa505300ba5d8e9f75c407b06
Crashneitor14 <pizzagames14@gmail.com>
2024-12-10 14:22:25 -0300

    prueba para server U

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     meta-description="Home meta description"
 >
 
-    <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">Connect CDE</h1>
+    <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">Connect CDE FACE</h1>
 
 </x-layouts.app>
 
