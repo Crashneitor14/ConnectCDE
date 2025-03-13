453657f9e380a587df5ed8b0dd3adebf55458286
Crashneitor14 <pizzagames14@gmail.com>
2025-03-13 12:15:55 -0300

    menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     title="Index"
     meta-description="Home meta description"
 >
-
+<h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">Bienvenido</h1>
     <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">Connect CDE FACE</h1>
 
 </x-layouts.app>
